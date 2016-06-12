0d9d41a281abb9b218499f0d7bb61815f26d81ee
Pablo Escobar <pescobar@facturecolombia.com>
2016-06-11 19:48:08 -0500

    timezone

---
+++ Azure/TimeZones/List-Windows-TimeZones.ps1
@@ -0,0 +1,32 @@
+﻿#requires -Version 4
+#requires -RunAsAdministrator
+<#
+.SYNOPSIS
+  Enter description here
+.EXAMPLE
+ Enter example here
+#>
+ 
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+ 
+$ErrorActionPreference = "Stop"  # Set Error Action to Stop
+$Script:ScriptVersion = "1.0"    # Script Version
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+Function Init {
+}
+
+#-----------------------------------------------------------[Initialize]-----------------------------------------------------------
+Clear-Host
+Init
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+Get-ChildItem -Path "HKLM:\Software\Microsoft\Windows Nt\CurrentVersion\Time Zones" | ? { $_.GetValue("Display")  -like '*UTC-05*' } | 
+	%{
+"# {1}
+WEBSITE_TIME_ZONE: '{0}'
+" -f $_.PSChildName, $_.GetValue("Display")
+	}
+
